5c1ad066d38ab536235462926858bd795d0130c5
klikli19 <103356373+klikli19@users.noreply.github.com>
2022-08-03 20:23:29 +0300

    Сделала задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1");
+        int clientOS = 0;
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
 
 
 
