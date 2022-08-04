1b9531d36f1cef99615572d712bc07fef1d149db
klikli19 <103356373+klikli19@users.noreply.github.com>
2022-08-04 19:44:17 +0300

    Сделала задание 2

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,19 @@ public class Main {
         } else {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
+        System.out.println();
 
-
-
+        System.out.println("Задание 2");
+        int clientOS1 = 1;
+        int clientDeviceYear = 2010;
+        if (clientOS1 == 1 && clientDeviceYear > 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS1 == 1 && clientDeviceYear <= 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS1 != 1 && clientDeviceYear <= 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
     }
 }
